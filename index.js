6f16004bf35da962b50e85c097e502a4756acfb0
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-05-18 17:24:53 +0500

    end

---
+++ index.js
@@ -0,0 +1,157 @@
+// if1
+
+// alert ('butun son berilgan agar son + bolsa 1 ga oshiring')
+// let a = +prompt ('a');
+// if (a>0){
+//     a++
+// }
+// console.log (a);
+
+// if2
+
+// alert ('butun son berilgan agar son + bolsa 1 ga oshiring aks xolda 2 ga kamaytiring')
+// let a = +prompt ('a');
+// if (a>0){
+//     a++
+// } else {
+//     a-=2;
+// }
+// console.log (a);
+
+// if3
+
+
+// alert ('butun son berilgan agar son + bolsa 1 ga oshiring aks xolda 2 ga kamaytiring 0 ga teng bolsa 10 chiqsin')
+// let num = +prompt ('num');
+// if (num>0){
+//     num++;
+// } else if(num==0){
+//     num+=10;
+// } else {
+//     num -=2;
+// }
+// console.log (num);
+
+// if4
+
+// alert ('3 ta son berilgan orasida nechta + son bor ekanligini aniqlang')
+// let num1 = +prompt ('num1');
+// let num2 = +prompt ('num2'); 
+// let num3 = +prompt ('num3');
+// let counter = 0;
+// if (num1>0){
+//     counter++;
+// } if (num2>0) {
+//     counter++;
+// }  if (num3>0) {
+//     counter++;
+// }
+// console.log (counter);
+
+// if5 
+
+// alert ('3 ta son berilgan orasida nechta + va nechta - son bor ekanligini aniqlang')
+// let num1 = +prompt ('num1');
+// let num2 = +prompt ('num2'); 
+// let num3 = +prompt ('num3');
+// let counter = 0;
+// let sum = 0;
+// if (num1>0){
+//     counter++;
+// } else {
+//     sum++;
+// } if (num2>0){
+//     counter++;
+// } else {
+//     sum++;
+// } if (num3>0){
+//     counter++;
+// } else {
+//     sum++;
+// }
+// console.log (counter);
+// console.log (sum);
+
+// if6
+
+// alert ('2ta butun son berilgan kattasini aniqlang')
+// let num1 = +prompt ('num1');
+// let num2 = +prompt ('num2'); 
+// if (num1>num2){
+//     console.log(num1);    
+// } else{
+//     console.log(num2);
+// }
+
+// if7
+
+// alert ('2ta butun son berilgan kichigini tartib raqamini aniqlang')
+// let num1 = +prompt ('num1');
+// let num2 = +prompt ('num2'); 
+// if (num1<num2){
+//     console.log(num1);    
+// } else{
+//     console.log(num2);
+// }
+
+// if8
+
+// alert ('2ta butun son berilgan avval kattasini keyin kichigini ekranga chiqaring')
+// let num1 = +prompt ('num1');
+// let num2 = +prompt ('num2'); 
+// if (num1>num2){
+//     console.log(num1);    
+// } else{
+//     console.log(num2);
+// }  if (num1<num2){
+//     console.log(num1);    
+// } else{
+//     console.log(num2);
+// }
+
+// if9
+
+// alert ('2 ta haqiqiy sonlar berilgan sonlarni shunday ozgartiringki 1-son kichik 2-+son katta bolsin')
+// let num1 = +prompt ('num1');
+// let num2 = +prompt ('num2'); 
+// console.log(num1+num2);
+
+// if10
+
+// alert ('2 ta son berilgan agar ular teng bolmasa ularning yeginisini ozlashtirsin teng bolsa 0 ni')
+// let num1 = +prompt ('num1');
+// let num2 = +prompt ('num2'); 
+// let num3;
+// if (num1 !== num2){
+//     num3 = num1+num2
+//     console.log (num3);
+// } else {
+//     console.log(num1);
+//     console.log(num2);
+// }
+
+// if11
+
+// alert ('2 ta son berilgan agar ular teng bolmasa kattasini ozlashtirsin teng bolsa 0 ni')
+// let num1 = +prompt ('num1');
+// let num2 = +prompt ('num2'); 
+// let num3;
+// if (num1==num2){
+//     console.log(num1+num2);
+// } if (num1>num2) {
+//     console.log(num1);
+// }  if (num1<num2) {
+//     console.log(num2);
+// }
+
+// if12 xato
+
+// alert ('3ta butun son berilgan kichigini aniqlang')
+// let num1 = +prompt ('num1');
+// let num2 = +prompt ('num2'); 
+// let num3 = +prompt ('num3'); 
+// if (num1<num2){
+//     console.log(num1);    
+// } if (num2<num3){
+//     console.log(num2);
+// }
